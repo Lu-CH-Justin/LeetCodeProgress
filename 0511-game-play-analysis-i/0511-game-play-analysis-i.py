5844e232497bee67a1b86a606cff0a4e62418324
Lu-CH-Justin <justinlu73@gmail.com>
2026-07-25 14:39:03 +0800

    Time: 286 ms (77.06%), Space: 68.3 MB (24.21%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.py
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
 def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
-    activity['first_login'] = activity.groupby('player_id')['event_date'].transform('min')
-    activity.drop_duplicates(subset = ['player_id'], inplace = True)
-    return activity[['player_id', 'first_login']]
+    df = activity.groupby('player_id')['event_date'].min()
+    return df.reset_index().rename(columns={'event_date': 'first_login'})
