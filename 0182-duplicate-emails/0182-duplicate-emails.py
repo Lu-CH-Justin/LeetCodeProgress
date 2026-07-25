c8c1db25aa496f270ea3039be9dd56ff414bd780
Lu-CH-Justin <justinlu73@gmail.com>
2026-07-25 13:33:57 +0800

    Time: 280 ms (56.5%), Space: 67 MB (72.02%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.py
+++ 0182-duplicate-emails/0182-duplicate-emails.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
-    df = person[person['email'].duplicated() == True]
+    df = person[person['email'].duplicated()]
     return df[['email']].drop_duplicates().rename(columns = {'email': 'Email'})
