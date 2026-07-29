b2a359852cabd2b6c258643f4aefd446142ef888
Lu-CH-Justin <justinlu73@gmail.com>
2026-07-29 15:43:25 +0800

    Time: 367 ms (20.57%), Space: 67.8 MB (96.03%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.py
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
     activities = activities.drop_duplicates().sort_values('product')
-    df = activities.groupby('sell_date').agg(num_sold = ('product','size'),products = ('product', ','.join)).reset_index()
+    df = activities.groupby('sell_date')['product'].agg(num_sold = 'size',products = ','.join).reset_index()
     return df
