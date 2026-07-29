be8521bbfa9880053a994aa4a4d89a534bfe1427
Lu-CH-Justin <justinlu73@gmail.com>
2026-07-29 18:13:25 +0800

    Time: 395 ms (8.59%), Space: 67.4 MB (55.67%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.py
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def find_primary_department(employee: pd.DataFrame) -> pd.DataFrame:
-    df = employee.groupby('employee_id')['department_id'].count().reset_index()
-    df = df[df['department_id'] == 1]['employee_id']
-    employee = employee[(employee['employee_id'].isin(df)) | ((~employee['employee_id'].isin(df)) & (employee['primary_flag'] == 'Y'))]
+    employee['count'] = employee.groupby('employee_id')['department_id'].transform('count')
+    print(employee)
+    employee = employee[(employee['count'] == 1) | ((employee['count'] > 1) & (employee['primary_flag'] == 'Y'))]
     return employee[['employee_id', 'department_id']]
