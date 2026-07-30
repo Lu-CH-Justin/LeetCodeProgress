f598f89f835b995d4b52ab006b9c4f2f8b6e0a11
Lu-CH-Justin <justinlu73@gmail.com>
2026-07-30 15:39:09 +0800

    Time: 266 ms (98.82%), Space: 68.2 MB (61.18%) - LeetHub

--- 3793-find-users-with-high-token-usage/3793-find-users-with-high-token-usage.py
+++ 3793-find-users-with-high-token-usage/3793-find-users-with-high-token-usage.py
@@ -3,7 +3,6 @@ import pandas as pd
 def find_users_with_high_tokens(prompts: pd.DataFrame) -> pd.DataFrame:
     prompts['avg_tokens'] = prompts.groupby('user_id')['tokens'].transform('mean')
     valid = prompts[prompts['tokens'] > prompts['avg_tokens']]['user_id']
-    print(valid)
     df = prompts.groupby('user_id')['tokens'].agg(prompt_count = 'count', avg_tokens = 'mean').round(2).reset_index()
     df = df[(df['prompt_count'] >= 3) & (df['user_id'].isin(valid))]
     return df.sort_values(['avg_tokens', 'user_id'], ascending = [False, True])
