e46fcc62cd008c6f821aaccf16bcde9b129f43b5
Lu-CH-Justin <justinlu73@gmail.com>
2026-07-30 15:16:27 +0800

    Time: 266 ms (87.14%), Space: 67.2 MB (8.57%) - LeetHub

---
+++ 3436-find-valid-emails/3436-find-valid-emails.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def find_valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    return users[users['email'].str.contains(r'^[a-zA-Z0-9_]+@[a-zA-Z]+\.com$')].sort_values('user_id')
