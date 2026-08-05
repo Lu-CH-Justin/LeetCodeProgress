8034e4f5eb90f53fd6f3a376d1857c90872dd04b
Lu-CH-Justin <justinlu73@gmail.com>
2026-08-05 16:42:59 +0800

    Time: 347 ms (5.39%), Space: 67.2 MB (38.34%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def exchange_seats(seat: pd.DataFrame) -> pd.DataFrame:
+    seat['student_next'] = seat['student'].shift(-1)
+    for i in range(1, len(seat), 2):
+        seat.iloc[i, 1] = seat.iloc[i - 1, 1]
+        seat.iloc[i - 1, 1] = seat.iloc[i - 1, 2]
+    return seat[['id', 'student']].sort_values('id')
