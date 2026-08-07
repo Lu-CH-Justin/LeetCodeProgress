224b1ef15cdc9b9df012ca4b51558de435af593a
Lu-CH-Justin <justinlu73@gmail.com>
2026-08-07 15:04:12 +0800

    Time: 1706 ms (10.16%), Space: 69.5 MB (31.33%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def monthly_transactions(transactions: pd.DataFrame) -> pd.DataFrame:
+    transactions['month'] = transactions['trans_date'].dt.strftime('%Y-%m')
+    df = transactions.groupby(['month','country'], dropna= False).agg(trans_count = ('id', 'count'), approved_count = ('state', lambda x: (x == 'approved').sum()),trans_total_amount = ('amount', 'sum'), approved_total_amount = ('amount', lambda x: x[transactions.loc[x.index, 'state'] == 'approved'].sum())).reset_index()
+    return df
