ace47a50d830dd160a4e950c0d0e4a954a29dd16
Lu-CH-Justin <justinlu73@gmail.com>
2026-08-07 15:10:07 +0800

    Time: 1609 ms (15.14%), Space: 69.9 MB (12.66%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.py
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.py
@@ -4,3 +4,11 @@ def monthly_transactions(transactions: pd.DataFrame) -> pd.DataFrame:
     transactions['month'] = transactions['trans_date'].dt.strftime('%Y-%m')
     df = transactions.groupby(['month','country'], dropna= False).agg(trans_count = ('id', 'count'), approved_count = ('state', lambda x: (x == 'approved').sum()),trans_total_amount = ('amount', 'sum'), approved_total_amount = ('amount', lambda x: x[transactions.loc[x.index, 'state'] == 'approved'].sum())).reset_index()
     return df
+
+    # A faster and better way
+    # first create the columns for approved
+    #transactions['approved'] = transactions['state'] == 'approved'
+    #transactions['approved_amount'] = 0
+    #transactions.loc[transactions['approved'], 'approved_amount'] = transactions['amount']
+    # then do groupby aggregation without lambda
+    #df = transactions.groupby(['month', 'country'], dropna=False).agg(trans_count=('id', 'count'),approved_count=('approved', 'sum'),trans_total_amount=('amount', 'sum'),approved_total_amount=('approved_amount', 'sum')).reset_index()
